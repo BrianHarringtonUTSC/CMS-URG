2b087dbd7bbbd48fb22e3594352a553bff5b4cc9
Malhar Pandya <41825230+malharpandya@users.noreply.github.com>
2023-06-17 20:30:37 -0400

    generated outline and 1 question for lecture 2

---
+++ VideoStyleDifferences/Lecture_2/counted_for_loop.py
@@ -0,0 +1,28 @@
+''' You want to make a cGPA calculator to calculate the average GPA over
+the 6 required courses in your first year. 
+
+Write a program that takes the GPA for 6 courses from the user, and prints
+the average of those GPAs. Don't forget to indicate to the user how they
+should input the GPAs by using a prompt when asking for input, as follows:
+    input("some prompt for the user")
+
+If the GPAs given by the user are: 4.0, 3.1, 3.0, 3.0, 4.0, 3.0
+The expected output is: 3.35
+
+Don't forget to check your code by running a few examples, including the one
+given above. '''
+
+''' PLEASE WRITE BELOW THIS COMMENT '''
+
+''' We are testing user input, and a for loop, sample solution follows.'''
+
+total_GPA = 0 # accumulator, we divide this by num_courses to get the cGPA
+for i in range(6):
+    # GPA can be fractional
+    user_prompt = "Enter course GPA: "
+    user_input = input(user_prompt)
+    course_GPA = float(user_input)
+    total_GPA += course_GPA
+cGPA = total_GPA / 6
+output_string = "Your cGPA is: " + str(cGPA)
+print(output_string)
---
+++ VideoStyleDifferences/outline.py
@@ -0,0 +1,16 @@
+'''
+This comment will be used to describe the problem that we (or the participants)
+wish to create a python script for. It will contain the following section
+(in order)
+1.     Context: This provides background for the question and introduces
+                an element of comprehension.
+2.      Action: This describes what the goal of the script it, strive
+                to be moderately explicit.
+3.     Example: One or more examples of input and expected output.
+4.       Hints: This is an optional part of the question, and aims to provides
+                a sense of direction towards some solution.
+5.    Reminder: This section is to remind students to double check their
+                work by running the script (and instructions on how to do so).
+'''
+
+''' Please write below this comment '''
